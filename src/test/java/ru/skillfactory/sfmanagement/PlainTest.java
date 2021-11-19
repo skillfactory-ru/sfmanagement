05ebacbf8520bb76301113d0ca97db562d2771c5
vrnsky <vrnsky@protonmail.com>
2021-11-20 01:52:38 +0800

    #1. Добавлено использование TestContainers. Исправлен неработающий тест

---
+++ src/test/java/ru/skillfactory/sfmanagement/PlainTest.java
@@ -0,0 +1,12 @@
+package ru.skillfactory.sfmanagement;
+
+
+import org.junit.Test;
+
+public class PlainTest extends SfmanagementApplicationTests {
+
+    @Test
+    public void test() {
+
+    }
+}
